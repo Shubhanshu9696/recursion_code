d450e71ba4e0a364feccfca659922b8d2292d64b
Shubhanshu9696 <92735584+Shubhanshu9696@users.noreply.github.com>
2022-05-31 01:43:56 +0530

    Update checkpelindrome.java

--- checkpelindrome.java
+++ checkpelindrome.java
@@ -16,26 +16,23 @@
     false
 */
 
-import java.lang.Thread.State;
 import java.util.Scanner;
 
 public class checkpelindrome{
     
+
+    static String reverse(String str){
+        if(str == null || str.length()<=1)
+            return str;
+        return reverse(str.substring(1))+str.charAt(0);
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        str = sc.nextLine();
-        // System.out.println(rev(str));
-        // String str1 = reverse(str);   System.out.println(str1);
-        // if(str.equals(str1))
-        //     System.out.println("string is pelindrome");
-        // else  System.out.println("string is not pelindrome");
-        
-        int start = 0, end = str.length() - 1;
-        while( start <  end ){
-            // swap
-            
-        }
-
+        String str = sc.nextLine();
+        String str1 = reverse(str);   
+        if(str.equals(str1))
+            System.out.println("string is pelindrome");
+        else  System.out.println("string is not pelindrome");
     }
 }
 
@@ -44,25 +41,25 @@ public class checkpelindrome{
 
      /* program to check any number is pelindrom or not*/
 
-// import java.util.Scanner;
-// public class checkpelindrome {
+import java.util.Scanner;
+public class checkpelindrome {
 
-//     static int pelindrome(int n,int temp ){
-//         if(n==0)
-//             return temp;
-//         temp = (temp*10) + (n%10);
-//         return pelindrome(n/10, temp) ;
+    static int pelindrome(int n,int temp ){
+        if(n==0)
+            return temp;
+        temp = (temp*10) + (n%10);
+        return pelindrome(n/10, temp) ;
         
-//     }
-//     public static void main(String[] args){
-//         Scanner sc = new Scanner (System.in);
-//         System.out.println("enter no. to check pelindrome");
-//         int n = sc.nextInt();
-//         int temp = pelindrome(n,0);
-//         if(temp == n)
-//         System.out.println("pelindrome number");
-//         else
-//         System.out.println("not a pelindroem number");
-//     }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter no. to check pelindrome");
+        int n = sc.nextInt();
+        int temp = pelindrome(n,0);
+        if(temp == n)
+        System.out.println("pelindrome number");
+        else
+        System.out.println("not a pelindroem number");
+    }
 
-// }
+}
